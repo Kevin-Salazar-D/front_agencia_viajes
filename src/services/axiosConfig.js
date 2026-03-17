9d0ff63ccc3a0be442dced3ffdd1fe74819c4782
Kevin-Salazar-D <kevinyahirsalazar342@gmail.com>
2026-03-17 00:54:39 -0600

    protegiendo vE

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.PROD 
-  ? "https://back-end-agencia-viajes.onrender.com/agenciaViajes" 
-  : "http://localhost:3000/agenciaViajes";
+const API_URL = import.meta.env.VITE_API_URL;
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
