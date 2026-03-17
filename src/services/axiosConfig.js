054534c3843e0e33ea34cc3984d226c78efe68d5
Kevin-Salazar-D <kevinyahirsalazar342@gmail.com>
2026-03-17 00:52:40 -0600

    Eliminando .env del rastreo de Git

--- src/services/axiosConfig.js
+++ src/services/axiosConfig.js
@@ -1,9 +1,8 @@
 import axios from "axios";
 
-const API_URL =
-  import.meta.env.VITE_API_URL_BASE ||
-  "http://localhost:3000/agenciaViajes";
-
+const API_URL = import.meta.env.PROD 
+  ? "https://back-end-agencia-viajes.onrender.com/agenciaViajes" 
+  : "http://localhost:3000/agenciaViajes";
 const api = axios.create({
   baseURL: API_URL,
   withCredentials: true,
